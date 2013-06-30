23812031b733d53ae551e843ebf86aaa95864fd3
Vo Minh Thu <noteed@gmail.com>
2013-06-30 21:05:16 +0200

    Cabalized program.

--- go.sh
+++ go.sh
@@ -1,8 +1,7 @@
 #! /bin/sh
-rm -rf main main.o main.hi main.tix .hpc bin/covered bin/covered.o bin/covered.hi
-rm -rf alternatives.html definitions.html expressions.html index.html
-rm -rf Main.hs*.html
+cabal clean
+rm -rf main main.o main.hi main.tix .hpc Main.hs.html
 ghc --make -fhpc main.hs || exit 1
-ghc --make -Wall bin/covered.hs || exit 1
+cabal configure && cabal build || exit 1
 ./main
-bin/covered main
+dist/build/covered/covered main
